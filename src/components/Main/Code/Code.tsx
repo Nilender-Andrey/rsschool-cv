f9d23c5e1c74e1fa12f72abca33878a1066db8be
Andrey Nilender <nilenderav@mail.ru>
2024-07-01 13:43:00 +0400

    feat: updated code

--- src/components/Main/Code/Code.tsx
+++ src/components/Main/Code/Code.tsx
@@ -7,12 +7,23 @@ function Code() {
       <h2 className="subtitle">code</h2>
       <div className="decoration decoration_color_dark-grey" />
       <pre className="code">
-        {'function maxMin(arr1, arr2) {\n'}
-        {
-          '  let res = arr1.map((item, index) => Math.abs(item - arr2[index]));\n'
-        }
-        {'  return [Math.max(...res), Math.min(...res)];\n'}
-        {'}'}
+        {`n = int(input())\n`}
+        {`matrix = [[int(item) for item in input().split()] for _ in range(n)]\n`}
+        {`s = set(range(1, n+1))\n`}
+        {`\n`}
+        {`for i in matrix:\n`}
+        {`    if s != set(i):\n`}
+        {`        print("NO")\n`}
+        {`        exit()`}
+        {`\n`}
+        {`matrix = [[matrix[j][i] for j in range(n)] for i in range(n)]\n`}
+        {`\n`}
+        {`for i in matrix:\n`}
+        {`    if s != set(i):\n`}
+        {`        print("NO")\n`}
+        {`        exit()`}
+        {`else:\n`}
+        {`    print("YES")`}
       </pre>
     </section>
   );
